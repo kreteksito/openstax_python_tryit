5c66db805d548c10252db9438d8eb0d24fc2db84
juan_carlos <mamanimamanijuancarlos05@gmail.com>
2026-09-08 22:10:49 -0400

    Initial commit: first Try It from OpenStax

---
+++ tryit.py
@@ -0,0 +1,8 @@
+print("Hello, What is your name?")
+name = input("")
+print("Nice to meet you, " + name + ".")
+
+
+print("What is your favorite music?")
+song = input()
+print("Cool! I like", song, "too.")
