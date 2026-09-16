b5eef8849537c4f4854886923b8a0efe1fb88769
juan_carlos <mamanimamanijuancarlos05@gmail.com>
2026-09-15 22:15:45 -0400

    add two tryit of Number basics

---
+++ tryit6.py
@@ -0,0 +1,7 @@
+# This program demonstrates the use of the type() function to determine the data type of different variables.
+int_a = 10
+float_a = 10.0
+string_a = "10"
+print (int_a, type (int_a))
+print (float_a, type (float_a))
+print (string_a, type (string_a))
---
+++ tryit7.py
@@ -0,0 +1,5 @@
+#Meters to feet
+meters = 10
+meter2feet = 3.28
+feet = meters * meter2feet
+print (feet)
