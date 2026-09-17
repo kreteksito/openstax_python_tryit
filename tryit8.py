c697f39f6e61a398e699dd2ca0967db61c679f7b
juan_carlos <mamanimamanijuancarlos05@gmail.com>
2026-09-16 20:09:53 -0400

    error messages

---
+++ tryit8.py
@@ -0,0 +1,11 @@
+#three errors, and correct until no errors are found
+#print("have a nice day!"
+print ("have a nice day!")
+
+#word = ("have a nice day!")
+#print("you tryped: ", wird)
+word = input("type something: ")
+print("you tryped: ", word)
+
+#    print("hello")
+print("Good bye")
---
+++ tryit9.py
@@ -0,0 +1,8 @@
+#Wrong symbols
+team1 = "Liverpool"
+score1 = "4"
+team2 = "Chelsea"
+score2 = "3"
+
+print(team1, "versus", team2)
+print("Final score:", score1, "to", score2)
