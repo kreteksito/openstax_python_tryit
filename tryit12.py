c6377b7fc93a600b0538a76139f29fd404c4c5ac
juan_carlos <mamanimamanijuancarlos05@gmail.com>
2026-09-18 22:12:52 -0400

    These are the final 'Try It' exercises for the '1 Statements' topic.

---
+++ tryit12.py
@@ -0,0 +1,18 @@
+""" Fun facts
+
+output the results of calculations arithmetic by a number input by the user
+Author: Juan Carlos Mamani Mamani
+"""
+#input a number from the user
+number = input("Enter a number: ")
+
+print()
+
+#print each operations arithmetic by a number input by the user
+print (number, "squared is", int(number)**2)
+print (number, "cubed is", int(number)**3)
+print ("One tenth of", number, "is", int(number)/10)
+print (number, "plus 10 is", int(number)+10)
+print (number, "minus 10 is", int(number)-10)
+
+
---
+++ tryit13.py
@@ -0,0 +1,16 @@
+"""Story Generator
+
+This program will generate a story based on the user's input of nouns and adjectives.
+"""
+
+# Enter 2 nouns and 2 adjectives
+noun1 = input("enter a noun: ")
+noun2 = input("Enter a noun: ")              
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+
+print()
+
+#print a story using the nouns and adjectives input by the user
+print("A vacation is whe you take a trip to come", adjective1, "place with your ", adjective2, "family.")
+print("Usually you go to some place that is near a/an", noun1, "or up on a/an", noun2, ".")
