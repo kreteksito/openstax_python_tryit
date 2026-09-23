fb5a1e5294864c3beb8cbbbbb394b487a29613fa
juan_carlos <mamanimamanijuancarlos05@gmail.com>
2026-09-22 22:45:55 -0400

    fix: variable decimal

--- two/tryit3.py
+++ two/tryit3.py
@@ -3,7 +3,7 @@
 The program print concatenation of strings as a number float but 'string' Ex: 2.3"""
 
 num = 2
-after_the_point = str(input("Enter a number for the decimal position after the point: "))
+decimal = str(input("Enter a number for the decimal position after the point: "))
 
 #print concatenation of strings
-print(str(num) + "." + after)
+print(str(num) + "." + decimal)
