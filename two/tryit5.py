4b686ea1d12fae2d0b49bc86cc3ce3374e8d8580
juan_carlos <mamanimamanijuancarlos05@gmail.com>
2026-09-23 22:17:21 -0400

    2.4 - Floating-point errors (2 tryit exercises)

---
+++ two/tryit5.py
@@ -0,0 +1,9 @@
+"""Inaccurate tips
+
+The following code calculates the tip amount, given a bill amount and the tip ratio."""
+
+bill = 33.33
+tip_ratio = 0.20
+tip_amount = tip_ratio * bill
+
+print("Tip =", tip_amount)
---
+++ two/tryit6.py
@@ -0,0 +1,14 @@
+"""Area of a triangle
+1. Calculate the area of a triangle with base = 7 and height = 3.5.
+2. Round the triangle's area to one decimal place.
+3. Round the triangle's area to the nearest integer value.
+"""
+# be use the formula for the area of a triangle
+base = 7
+height = 3.5
+area = base * height/2
+
+# print using round() function
+print ("Triangle's area:", area)
+print ("Triangle's area rounded to one decimal place:", round(area,1))
+print ("Triangle's area rounded to the nearest integer value:", round(area))
